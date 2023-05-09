9f3552c17b6a2f05d4ca73b7e7c722b37a3c6e4d
c0b2204443 <c0b2204443@edu.teu.ac.jp>
2023-05-09 15:14:55 +0900

    練習２ミス

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -162,6 +162,7 @@ def main():
             if bomb is not None and beam._rct.colliderect(bomb._rct): 
                 beam = None
                 bomb = None
+                bird.change_img(6,screen)
         
         
         pg.display.update()
